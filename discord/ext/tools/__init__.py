512c423b3f8df1fd821d731b0f60670e78939b2b
Developer Anonymous <kibandhonor@gmail.com>
2023-11-14 21:41:02 +0100

    Deleted tests.py and updated _discord to -> dicord

---
+++ discord/ext/tools/__init__.py
@@ -0,0 +1,12 @@
+"""
+discord.ext.tools
+~~~~~~~~~~~~~~~~~
+
+Miscellaneous utils for Discord, treated as extensions.
+"""
+
+__author__: str = "Developer-Anony"
+__license__: str = "MIT"
+__copyright__: str = "(c) Developer-Anony // 2023"
+
+from .help_commands import AppHelpCommand

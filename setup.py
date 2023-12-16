bb7a68a66b14e7378de1f1401026eb4c977170b6
Developer Anonymous <kibandhonor@gmail.com>
2023-12-16 12:42:58 +0100

    Udated URL

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="discord-ext-tools",
     version="1.0.0",
     packages=["discord.ext.tools"],
-    url="https://github.com/Developer-Anony/discord-ext-tools",
+    url="https://github.com/DA-344/discord-ext-tools",
     license="MIT",
     author="Dev Anony",
     author_email="dev.anony.8593@gmail.com",

fa4777bbe5b8028ed681a6a61b1be96411c7bf63
Developer Anonymous <kibandhonor@gmail.com>
2024-09-05 17:18:56 +0200

    Bump version 0.1.0 -> 0.2.0a

--- discord_tools/__init__.py
+++ discord_tools/__init__.py
@@ -8,7 +8,7 @@ Miscellaneous tools for discord.py.
 __author__ = "DA344"
 __license__ = "MIT"
 __copyright__ = "(c) 2024 present, DA344"
-__version__ = "0.1.0"
+__version__ = "0.2.0a"
 
 
 from .models import *
